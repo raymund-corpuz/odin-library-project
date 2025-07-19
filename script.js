fb95c674e3b9ad0c8de34472b58f7e8e39a82105
raymund-corpuz <raymundcorpuz86@gmail.com>
2025-07-19 21:30:03 +0800

    odin-library: display all the books in the mobile-screen

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
 const URL = "https://openlibrary.org/search.json";
 const imgURL = "https://covers.openlibrary.org/b/id/";
 const imageDOM = document.querySelector("#image");
+const main = document.querySelector(".main-section");
 
 const que = "Harry Potter";
 
 const newQue = que.toLowerCase().replaceAll(" ", "+");
 
-console.log(newQue);
-
-console.log(URL);
 async function getData() {
   const respose = await fetch(`${URL}?q=${newQue}`);
   const data = await respose.json();
 
-  console.log(data);
+  const books = data.docs;
 
   console.log(data.docs[0].title);
   console.log(data.docs[0].author_name);
@@ -22,11 +20,30 @@ async function getData() {
   console.log(data.docs[0].cover_i);
   const imgQue = data.docs[0].cover_i;
 
-  //   getImage(imgQue);
+  books.map((book) => getImage(book));
 }
 
-// async function getImage(imgQue) {
-//   imageDOM.src = `${imgURL}${imgQue}-L.jpg`;
-// }
+function getImage(element) {
+  console.log(typeof element);
+  const bookContainer = document.createElement("div");
+  const titleBook = document.createElement("p");
+  const authorBook = document.createElement("p");
+  const publishBook = document.createElement("p");
+  const image = document.createElement("img");
+  bookContainer.classList.add("book-container");
+
+  titleBook.textContent = `Title: ${element.title}`;
+  authorBook.textContent = `Author: ${element.author_name}`;
+  publishBook.textContent = `Publish: ${element.first_publish_year}`;
+  image.src = `${imgURL}${element.cover_i}-S.jpg`;
+  image.style.width = "75px";
+  image.style.height = "100px";
+
+  main.appendChild(bookContainer);
+  bookContainer.appendChild(image);
+  bookContainer.appendChild(titleBook);
+  titleBook.appendChild(authorBook);
+  titleBook.appendChild(publishBook);
+}
 
 getData();
